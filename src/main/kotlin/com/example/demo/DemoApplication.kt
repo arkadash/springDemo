e2cf3d6703aa73bf11cc4bc040fa387bada09d6c
Moshe Arkadash <moshe.arkadash@fiverr.com>
2021-05-27 00:46:15 +0300

    After creating The Bean

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,10 +1,19 @@
 package com.example.demo
 
+import com.hello.service.ConsoleHelloService
+import com.hello.service.HelloService
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class DemoApplication
+class DemoApplication {
+
+	@Bean
+	fun helloService(): HelloService {
+		return ConsoleHelloService()
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
