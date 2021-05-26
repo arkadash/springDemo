97edc4e8d3cd0a316a284a7c45c9fefc75828ea0
Moshe Arkadash <moshe.arkadash@fiverr.com>
2021-05-27 00:47:27 +0300

    Without @SpringBoot Annotation (Missing the Hello Message)

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 
-@SpringBootApplication
+//@SpringBootApplication
 class DemoApplication {
 
 	@Bean
